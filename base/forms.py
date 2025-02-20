8b11eb242b9e19e638ceb5f842434eeb77352ca4
Abdul Saboor <saboorking4@gmai.com>
2025-02-20 21:01:08 +0500

    Initial commit

---
+++ base/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from .models import Room , User
+
+
+class Roomform(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
+        exclude = ['host', 'participants']
+
+class Userform(ModelForm):
+    class Meta:
+        model = User
+        fields = ['avatar', 'name', 'username', 'email', 'bio']
+        
+       
